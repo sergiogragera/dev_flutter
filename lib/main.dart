2d286162ff63c4f2e4dc80540025ef0174ba68e8
Sergio Gragera Camino <sergiogragera@gmail.com>
2019-02-10 11:41:45 +0100

    Add open in browser action to bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +10,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final flutterWebViewPlugin = FlutterWebviewPlugin();
     const int WEB_NAVIGATION_BAR_COLOR = 0xFFfdf9f3;
+
+    _launchURL() async {
+      var url =
+          await flutterWebViewPlugin.evalJavascript("window.location.href");
+      if (await canLaunch(url)) {
+        await launch(url);
+      }
+    }
 
     return new MaterialApp(
       routes: {
@@ -36,7 +45,14 @@ class MyApp extends StatelessWidget {
                           onPressed: () {
                             flutterWebViewPlugin.goForward();
                           },
-                        )
+                        ),
+                        new Spacer(),
+                        IconButton(
+                          icon: const Icon(Icons.open_in_browser),
+                          onPressed: () {
+                            _launchURL();
+                          },
+                        ),
                       ],
                     ),
                   ),
