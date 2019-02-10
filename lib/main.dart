30960e6f2f42fb9e8c3fcd6c451e7e7a75b5622e
Sergio Gragera Camino <sergiogragera@gmail.com>
2019-02-10 11:28:26 +0100

    Fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final flutterWebViewPlugin = FlutterWebviewPlugin();
-    const int WEB_NAVIGATIONBAR_COLOR = 0xFFfdf9f3;
+    const int WEB_NAVIGATION_BAR_COLOR = 0xFFfdf9f3;
 
     return new MaterialApp(
       routes: {
         "/": (_) => new Container(
-              color: Color(WEB_NAVIGATIONBAR_COLOR),
+              color: Color(WEB_NAVIGATION_BAR_COLOR),
               child: new SafeArea(
                 bottom: false,
                 child: new WebviewScaffold(
                   url: "https://dev.to",
                   userAgent: "DEV-Native-ios",
                   bottomNavigationBar: new BottomAppBar(
-                    color: Color(WEB_NAVIGATIONBAR_COLOR),
+                    color: Color(WEB_NAVIGATION_BAR_COLOR),
                     child: new Row(
                       mainAxisSize: MainAxisSize.min,
                       mainAxisAlignment: MainAxisAlignment.start,
