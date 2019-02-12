364d306e3d60776ab1fc69d35a41d87c92eb063d
sergiogragera <sergiogragera@gmail.com>
2019-02-12 15:35:13 +0100

    Add connectivity snackbar alert

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:connectivity/connectivity.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flushbar/flushbar.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,6 +28,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final flutterWebViewPlugin = FlutterWebviewPlugin();
   final int WEB_NAVIGATION_BAR_COLOR = 0xFFfdf9f3;
+  ConnectivityResult _connectivityResult;
 
   _launchURL() async {
     var url = await flutterWebViewPlugin.evalJavascript("window.location.href");
@@ -44,8 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     initConnectivity();
-    _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    _connectivitySubscription = _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
 
   @override
@@ -54,24 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initConnectivity() async {
-    ConnectivityResult result;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      result = await _connectivity.checkConnectivity();
+      _connectivityResult = await _connectivity.checkConnectivity();
     } on PlatformException catch (e) {
       print(e.toString());
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) {
-      return;
-    }
-
-    _updateConnectionStatus(result);
   }
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -85,51 +72,63 @@ class _MyHomePageState extends State<MyHomePage> {
         bottom: false,
         child: new WebviewScaffold(
           url: "https://dev.to",
-          userAgent: "DEV-Native-ios",
-          bottomNavigationBar: new BottomAppBar(
-            color: Color(WEB_NAVIGATION_BAR_COLOR),
-            child: new Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.arrow_back_ios),
-                  onPressed: () {
-                    flutterWebViewPlugin.goBack();
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.arrow_forward_ios),
-                  onPressed: () {
-                    flutterWebViewPlugin.goForward();
-                  },
-                ),
-                new Spacer(),
-                IconButton(
-                  icon: const Icon(Icons.open_in_browser),
-                  onPressed: () {
-                    _launchURL();
-                  },
-                ),
-              ],
+          userAgent: "DEV-Native-ios"
+        ),
+      ),
+      bottomNavigationBar: new BottomAppBar(
+        color: Color(WEB_NAVIGATION_BAR_COLOR),
+        child: new Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                flutterWebViewPlugin.goBack();
+              },
             ),
-          ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: () {
+                flutterWebViewPlugin.goForward();
+              },
+            ),
+            new Spacer(),
+            IconButton(
+              icon: const Icon(Icons.open_in_browser),
+              onPressed: () {
+                _launchURL();
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 
   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-    switch (result) {
-      case ConnectivityResult.wifi:
-      case ConnectivityResult.mobile:
-      case ConnectivityResult.none:
-          _scaffoldKey.currentState.showSnackBar(new SnackBar(
-              content: new Text(result.toString())
-          ));
-        break;
-      default:
-        break;
+    if (_connectivityResult != result) {
+      var text;
+      var backgroundColor;
+      print(result);
+      switch (result) {
+        case ConnectivityResult.wifi:
+          text = "Re-connected to WiFi";
+          backgroundColor = Colors.greenAccent;
+          break;
+        case ConnectivityResult.mobile:
+          text = "Re-connected to Cellular";
+          backgroundColor = Colors.greenAccent;
+          break;
+        case ConnectivityResult.none:
+          text = "Network not reachable";
+          backgroundColor = Colors.redAccent;
+          break;
+        default:
+          break;
+      }
+      _scaffoldKey.currentState.showSnackBar(new SnackBar(content: new Text(text), backgroundColor: backgroundColor));
     }
+    _connectivityResult = result;
   }
 }
